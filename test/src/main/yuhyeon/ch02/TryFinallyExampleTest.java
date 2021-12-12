070a2117aca2371e943ebf1c8c04acac9ecf70e3
baekho <klyhyeon@gmail.com>
2021-12-12 14:14:12 +0900

    feat: item15_클래스와_멤버의_접근_권한을_최소화하라

---
+++ test/src/main/yuhyeon/ch02/TryFinallyExampleTest.java
@@ -0,0 +1,18 @@
+package main.yuhyeon.ch02;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import yuhyeon.ch02.item09.TryFinallyExample;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class TryFinallyExampleTest {
+
+    @Test
+    @DisplayName("Suppressed_테스트")
+    void Suppressed_테스트() {
+        assertThatThrownBy(() -> TryFinallyExample.firstLineOfFileTryResources("testPath", "a"))
+                .isInstanceOf(NullPointerException.class).hasMessageContaining("null");
+
+    }
+}
