f60b9b51705f53493aefc547d268dbd4a8144688
yuhyeonct <yuhyeon.lee@cointelli.com>
2022-02-06 19:33:39 +0900

    feat: 아이템50_적시에_방어적_복사본을_만들라_yh

---
+++ src/main/yuhyeon/ch08/Main.java
@@ -0,0 +1,14 @@
+package yuhyeon.ch08;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Date start = new Date();
+        Date end = new Date();
+        Period p = new Period(start, end);
+        p.getEnd().setTime(0000L); //Date의 setTime을 통해 여전히 장난칠 수 있습니다.
+    }
+}
